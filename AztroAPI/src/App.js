65e2aab54770a0e5e39727efb668b24ed1c7ea51
Mafalda-hub <78922064+Mafalda-hub@users.noreply.github.com>
2022-03-09 16:57:03 +0000

    register and login

--- AztroAPI/src/App.js
+++ AztroAPI/src/App.js
@@ -3,13 +3,19 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import About from './components/About';
+import Register from './components/Register';
+import Login from './components/Login';
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
