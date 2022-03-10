8ebaadf8e492c9f4dc8576511e2520c7ef03eb85
Tom Riley <you@example.com>
2022-03-10 11:12:32 +0000

    pushing some CSS changes to daily horoscope page

--- AztroAPI/src/components/DailyHoroscope.js
+++ AztroAPI/src/components/DailyHoroscope.js
@@ -4,9 +4,9 @@ const DailyHoroscope = () => {
   return (
     <section className='dh-section'>
       <div className='dh-container'>
-        <div className='dh-yesterday'>Yesterday</div>
+        {/* <div className='dh-yesterday'>Yesterday</div> */}
         <div className='dh-today'>Today</div>
-        <div className='dh-tomorrow'>Tomorrow</div>
+        {/* <div className='dh-tomorrow'>Tomorrow</div> */}
       </div>
     </section>
   );
