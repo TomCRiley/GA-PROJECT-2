b1480b1f809c7b68aa2a699a775102d8df2e12e1
Tristan Hall <tristan.hall@procensus.com>
2022-03-10 18:50:49 +0000

    fixed fropdown menu

--- AztroAPI/src/components/DailyHoroscope.js
+++ AztroAPI/src/components/DailyHoroscope.js
@@ -1,33 +1,74 @@
 import React from 'react';
 import Dropdown from './Dropdown';
+import { getSingleSign } from '../lib/api';
+
+const signs = [
+  'Aries',
+  'Taurus',
+  'Gemini',
+  'Cancer',
+  'Leo',
+  'Virgo',
+  'Libra',
+  'Scorpio',
+  'Sagittarius',
+  'Capricorn',
+  'Aquarius',
+  'Pisces'
+];
 
 const DailyHoroscope = () => {
+  const [sign, setSign] = React.useState('');
+  const [horoscope, setHoroscope] = React.useState(null);
+
+  const getRequestUrl = (starSign) => {
+    setSign(starSign);
+  };
+
+  React.useEffect(() => {
+    if (sign) {
+      async function getData(params) {
+        const { data } = await getSingleSign(sign);
+        setHoroscope(data);
+      }
+
+      getData();
+    }
+  }, [sign]);
+
   return (
     <div>
       <h1>What's your sign?</h1>
-      {/* <Dropdown formLabel="Choose a sign" buttonText="Send sign" action="/">
-          <Option selected value="Click to see options" />
-          <Option value="Aries" />
-          <Option value="Taurus" />
-          <Option value="Gemini" />
-          <Option value="Cancer" />
-          <Option value="Leo" />
-          <Option value="Virgo" />
-          <Option value="Libra" />
-          <Option value="Scorpio" />
-          <Option value="Sagittarius" />
-          <Option value="Capricorn" />
-          <Option value="Aquarius" />
-          <Option value="Pisces" />
-        </Dropdown> */}
-      {/* </div> */}
-      {/* // <section className='dh-section'> */}
-      {/* <div className='dh-container'> */}
-      {/* <div className='dh-yesterday'>Yesterday</div> */}
-      {/* <div className='dh-today'>Today</div> */}
-      {/* <div className='dh-tomorrow'>Tomorrow</div> */}
-      //{' '}
+      <Dropdown
+        handleClick={getRequestUrl}
+        formLabel="Choose a sign"
+        buttonText="Send sign"
+        action="/"
+        signs={signs}
+      >
+        <option selected value="Click to see options" />
+        <option value="Aries" />
+        <option value="Taurus" />
+        <option value="Gemini" />
+        <option value="Cancer" />
+        <option value="Leo" />
+        <option value="Virgo" />
+        <option value="Libra" />
+        <option value="Scorpio" />
+        <option value="Sagittarius" />
+        <option value="Capricorn" />
+        <option value="Aquarius" />
+        <option value="Pisces" />
+      </Dropdown>
+
+      {!!horoscope && <p>{horoscope.description}</p>}
     </div>
+    // <section className='dh-section'>
+    // <div className='dh-container'>
+    // <div className='dh-yesterday'>Yesterday</div>
+    // <div className='dh-today'>Today</div>
+    // <div className='dh-tomorrow'>Tomorrow</div>
+    // </div>
     // </section>
   );
 };
--- AztroAPI/src/components/Dropdown.js
+++ AztroAPI/src/components/Dropdown.js
@@ -1,41 +1,42 @@
 import React from 'react';
 
-const Dropdown = () => {
+const Dropdown = ({ signs, handleClick }) => {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const handleDropdownClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <section>
-      <div class="dropdown is-active">
-        <div class="dropdown-trigger">
+      <div className={isOpen ? 'dropdown is-active' : 'dropdown'}>
+        <div className="dropdown-trigger">
           <button
-            class="button"
+            className="button"
             aria-haspopup="true"
             aria-controls="dropdown-menu"
+            onClick={handleDropdownClick}
           >
-            <span>Dropdown button</span>
-            <span class="icon is-small">
-              <i class="fas fa-angle-down" aria-hidden="true"></i>
+            <span className="icon is-small">
+              <i className="fas fa-angle-down" aria-hidden="true"></i>
             </span>
+            What is your sign?
           </button>
         </div>
-        <div class="dropdown-menu" id="dropdown-menu" role="menu">
-          <div class="dropdown-content">
-            <a
-              href="https://aztro.sameerkumar.website?sign=aries"
-              class="dropdown-item"
-            >
-              Aries
-            </a>
-            <a
-              href="https://aztro.sameerkumar.website?sign=taurus"
-              class="dropdown-item"
-            >
-              Taurus
-            </a>
-            <a
-              href="https://aztro.sameerkumar.website?sign=gemini"
-              class="dropdown-item"
-            >
-              Gemini
-            </a>
+        <div className="dropdown-menu" id="dropdown-menu" role="menu">
+          <div className="dropdown-content">
+            {signs.map((sign) => (
+              <button
+                key={sign}
+                onClick={() => {
+                  handleClick(sign);
+                  handleDropdownClick();
+                }}
+                className="dropdown-item"
+              >
+                {sign}
+              </button>
+            ))}
           </div>
         </div>
       </div>
