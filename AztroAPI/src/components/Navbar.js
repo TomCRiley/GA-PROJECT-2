7c35463dcf24435e4378f638dbd1141f70857007
Tom Riley <you@example.com>
2022-03-10 18:55:07 +0000

    merging

--- AztroAPI/src/components/Navbar.js
+++ AztroAPI/src/components/Navbar.js
@@ -5,41 +5,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <header>
-// <<<<<<< css
-//       <nav className='navbar is-dark'>
-//         <div className='container'>
-//           <div className='navbar-brand'>
-//             {/* <img src='./assets/black-cat-logo-small.png' alt='black cat logo' /> */}
-//             <Link to='/' className='navbar-item'>
-// =======
-//       <nav className="navbar is-dark">
-//         <div className="container">
-//           <div className="navbar-brand">
-//             <Link to="/" className="navbar-item">
-// >>>>>>> main
-              Home
-            </Link>
-            <Link to="/dailyhoroscope" className="navbar-item">
-              Daily Horoscope
-            </Link>
-            <Link to="about" className="navbar-item">
-              About
-            </Link>
-            <Link to="/register" className="navbar-item">
-              Register
-            </Link>
-            <Link to="/login" className="navbar-item">
-              Login
-            </Link>
-            <Link to="/horoscope/:name" className="navbar-item">
-              Card
-            </Link>
-            {/* <Link to="horoscopeshow" className="navbar-item">
-              Card
-            </Link> */}
-          </div>
-        </div>
-      </nav>
+      <nav className='navbar is-dark'></nav>
     </header>
   );
 };
