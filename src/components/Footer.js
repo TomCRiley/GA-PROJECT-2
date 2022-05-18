1bba47eec7f982e5e3da5b993e59540ce22cc3a9
Tom Riley <tom.riley@outlook.com>
2022-05-18 10:21:55 +0100

    added github links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,16 @@ import React from 'react';
 const Footer = () => {
   return (
     <div className='footer-styles'>
-      <h3 id='made-by'>Made at ❤️General Assembly❤️ by Ana & Tom</h3>
+      <h3 id='made-by'>
+        Made at ❤️General Assembly❤️ by
+        <a className='github' href='https://github.com/Mafalda-hub'>
+          Ana
+        </a>
+        &
+        <a className='github' href='https://github.com/TomCRiley'>
+          Tom
+        </a>
+      </h3>
     </div>
   );
 };
